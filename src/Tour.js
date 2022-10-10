6f77960329cfcf70a7476565736c1606b234315b
Shajwan <shajwannamiqsalh@gmail.com>
2022-10-10 16:58:07 +0300

    update header

--- src/Tour.js
+++ src/Tour.js
@@ -6,8 +6,8 @@ import React from 'react'
 
    return (
      <div>
-       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-64 lg:py-20">
-         <div className="   ">
+       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-60 lg:py-10">
+         <div className="">
            <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
              <img src={image} className="object-cover w-full h-64" alt="" />
              <div className="p-5  border border-t-0">
--- src/Tours.js
+++ src/Tours.js
@@ -5,12 +5,18 @@ import Tour from './Tour';
 const Tours = ({ tours, removeTour }) => {
   return (
     <div>
-      <p>Our Tours</p>
-      <div className="underline"></div>
+      <div class="mt-5 relative flex px-5 lg:px-72 py-5 items-center">
+        <div class="flex-grow border-t border-gray-800"></div>
+        <span class="flex-shrink mx-4 font-bold text-2xl uppercase text-emerald-900">
+          Our Tours
+        </span>
+        <div class="flex-grow border-t border-gray-400"></div>
+      </div>
+       
 
       <div>
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} removeTour={removeTour} ></Tour>;
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
         })}
       </div>
     </div>
